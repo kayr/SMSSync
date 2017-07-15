768b86fc5eaa645de5785894f824924628fca2ef
kayr <noryak1234@gmail.com>
2017-07-15 16:33:21 +0300

    make default sim selection more predicatable... i.e always select default as first sim

--- smssync/src/main/java/org/addhen/smssync/messages/SimUtil.java
+++ smssync/src/main/java/org/addhen/smssync/messages/SimUtil.java
@@ -1,15 +1,5 @@
 package org.addhen.smssync.messages;
 
-import static org.addhen.smssync.messages.ReflectionUtils.invokeStaticMethod;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.List;
-
-import android.telephony.SubscriptionInfo;
-import org.addhen.smssync.util.Util;
-
 import android.app.PendingIntent;
 import android.content.Context;
 import android.database.Cursor;
@@ -17,210 +7,238 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.IBinder;
 import android.telephony.SmsManager;
+import android.telephony.SubscriptionInfo;
 import android.telephony.SubscriptionManager;
 import android.util.Log;
+import org.addhen.smssync.util.Util;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.addhen.smssync.messages.ReflectionUtils.invokeStaticMethod;
 
 /**
  * Created by Apipas on 6/4/15.
  */
 public class SimUtil {
 
-	public static boolean sendSMS(Context ctx, String toNum, ArrayList<String> smsTextlist,
-			ArrayList<PendingIntent> sentIntentList, ArrayList<PendingIntent> deliveryIntentList) {
+    public static boolean sendSMS(Context ctx, String toNum, ArrayList<String> smsTextlist,
+                                  ArrayList<PendingIntent> sentIntentList, ArrayList<PendingIntent> deliveryIntentList) {
 
-		List<SimInfo> simInfoList = getSIMInfo(ctx);
-		if (simInfoList.size() > 1) {
+        List<SimInfo> simInfoList = getSIMInfo(ctx);
+        if (simInfoList.size() > 1) {
 
-			boolean isAirtel = toNum.startsWith("+25670") || toNum.startsWith("+25675");
-			boolean isMtn = toNum.startsWith("+25677") || toNum.startsWith("+25678");
+            boolean isAirtelNumber = toNum.startsWith("+25670") || toNum.startsWith("+25675");
+            boolean isMtnNumber    = toNum.startsWith("+25677") || toNum.startsWith("+25678");
 
-			SimInfo airSim = null, mtnSim = null;
+            List<SimInfo> airSimList = new ArrayList<>(2), mtnSimList = new ArrayList<>(2);
 
-			for (SimInfo sim : simInfoList) {
-				String displayName = sim.getDisplay_name().toLowerCase();
-				if (displayName.contains("mtn")) {
-					mtnSim = sim;
-				} else if (displayName.contains("warid") || displayName.contains("celtel")) {
-					airSim = sim;
-				}
-			}
+            for (SimInfo sim : simInfoList) {
+                if (isMtnSim(sim)) {
+                    mtnSimList.add(sim);
+                } else if (isWaridSim(sim)) {
+                    airSimList.add(sim);
+                }
+            }
 
-			SimInfo selectedSim = null;
-			if (isMtn && null != mtnSim) {
-				selectedSim = mtnSim;
-			} else if (isAirtel && null != airSim) {
-				selectedSim = airSim;
-			}
+            SimInfo selectedSim = null;
+            if (isMtnNumber && !mtnSimList.isEmpty()) {
+                selectedSim = firstSim(mtnSimList);
+            } else if (isAirtelNumber && !airSimList.isEmpty()) {
+                selectedSim = firstSim(airSimList);
+            }
 
-			if (selectedSim != null) {
-				Util.logActivities(ctx,
-						"DualSim: Sending SMS to(" + toNum + ") Using: [" + selectedSim.getDisplay_name() + "]");
-				return sendSMS(ctx, selectedSim.getSlot(), toNum, smsTextlist, sentIntentList, deliveryIntentList);
-			}
+            if (selectedSim == null) {
+                selectedSim = firstSim(simInfoList);
+            }
 
-			return sendSMSWithDefault(toNum, smsTextlist, sentIntentList, deliveryIntentList);
 
-		} else {
-			return sendSMSWithDefault(toNum, smsTextlist, sentIntentList, deliveryIntentList);
-		}
+            String message = String.format("DualSim: SMS to(%s) Using: [%s] Slot(%s)", toNum, selectedSim.getDisplay_name(), selectedSim.getSlot());
+            Util.logActivities(ctx, message);
+            return sendSMS(ctx, selectedSim.getSlot(), toNum, smsTextlist, sentIntentList, deliveryIntentList);
 
-	}
 
-	private static boolean sendSMSWithDefault(String toNum, ArrayList<String> smsTextlist,
-			ArrayList<PendingIntent> sentIntentList, ArrayList<PendingIntent> deliveryIntentList) {
-		SmsManager sms = SmsManager.getDefault();
-		sms.sendMultipartTextMessage(toNum, null, smsTextlist, sentIntentList, deliveryIntentList);
-		return true;
-	}
+        } else {
+            return sendSMSWithDefault(toNum, smsTextlist, sentIntentList, deliveryIntentList);
+        }
 
-	private static boolean sendSMS(Context ctx, int simID, String toNum, ArrayList<String> smsTextlist,
-			ArrayList<PendingIntent> sentIntentList, ArrayList<PendingIntent> deliveryIntentList) {
+    }
 
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
-			SubscriptionInfo subscriptionInfo = SubscriptionManager.from(ctx).getActiveSubscriptionInfoForSimSlotIndex(simID);
-			int subscriptionId = subscriptionInfo.getSubscriptionId();
-			SmsManager smsManagerForSubscriptionId = SmsManager.getSmsManagerForSubscriptionId(subscriptionId);
-			smsManagerForSubscriptionId.sendMultipartTextMessage(toNum, null, smsTextlist, sentIntentList,
-					deliveryIntentList);
+    private static SimInfo firstSim(List<SimInfo> simInfos) {
+        SimInfo firstSlot = null;
+        for (SimInfo simInfo : simInfos) {
+            if (firstSlot == null || simInfo.getSlot() < firstSlot.getSlot()) {
+                firstSlot = simInfo;
+            }
+        }
+        return firstSlot;
+    }
 
-		} else {
+    private static boolean isWaridSim(SimInfo sim) {
+        return sim.getDisplay_name().toLowerCase().contains("warid") || sim.getDisplay_name().toLowerCase().contains("celtel");
+    }
 
-			boolean sentWithK3Api = sentSMSWithLenovoK3API(ctx, simID, toNum, smsTextlist, sentIntentList,
-					deliveryIntentList);
+    private static boolean isMtnSim(SimInfo sim) {
+        return sim.getDisplay_name().toLowerCase().contains("mtn");
+    }
 
-			if (sentWithK3Api) {
-				Util.logActivities(ctx, "DualSim: Message to(" + toNum + ")  Sent With LenovoK3 API");
-				return true;
-			}
+    private static boolean sendSMSWithDefault(String toNum, ArrayList<String> smsTextlist,
+                                              ArrayList<PendingIntent> sentIntentList, ArrayList<PendingIntent> deliveryIntentList) {
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendMultipartTextMessage(toNum, null, smsTextlist, sentIntentList, deliveryIntentList);
+        return true;
+    }
 
-			Util.logActivities(ctx, "DualSim: SMS Failed: LenovoK3 API 1");
+    private static boolean sendSMS(Context ctx, int simID, String toNum, ArrayList<String> smsTextlist,
+                                   ArrayList<PendingIntent> sentIntentList, ArrayList<PendingIntent> deliveryIntentList) {
 
-			boolean sentWithReflection = sendMultipartTextSMS(ctx, simID, toNum, null, smsTextlist, sentIntentList,
-					deliveryIntentList);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
+            SubscriptionInfo subscriptionInfo            = SubscriptionManager.from(ctx).getActiveSubscriptionInfoForSimSlotIndex(simID);
+            int              subscriptionId              = subscriptionInfo.getSubscriptionId();
+            SmsManager       smsManagerForSubscriptionId = SmsManager.getSmsManagerForSubscriptionId(subscriptionId);
+            smsManagerForSubscriptionId.sendMultipartTextMessage(toNum, null, smsTextlist, sentIntentList,
+                    deliveryIntentList);
 
-			if (sentWithReflection) {
-				Util.logActivities(ctx, "DualSim: Message to(" + toNum + ") Sent With Reflection API 1");
-				return true;
-			}
-			Util.logActivities(ctx, "SMS Failed: Reflection API 1");
+        } else {
 
-		}
+            boolean sentWithK3Api = sentSMSWithLenovoK3API(ctx, simID, toNum, smsTextlist, sentIntentList,
+                    deliveryIntentList);
 
-		Util.logActivities(ctx, "Failed to send SMS to(" + toNum + ")  with DualSimAPi..Falling back to default");
-		sendSMSWithDefault(toNum, smsTextlist, sentIntentList, deliveryIntentList);
-		return true;
-	}
+            if (sentWithK3Api) {
+                //Util.logActivities(ctx, "DualSim: Message to(" + toNum + ")  Sent With LenovoK3 API");
+                return true;
+            }
 
-	private static boolean sentSMSWithLenovoK3API(Context ctx, int simID, String toNum, ArrayList<String> smsTextlist,
-			ArrayList<PendingIntent> sentIntentList, ArrayList<PendingIntent> deliveryIntentList) {
+            Util.logActivities(ctx, "DualSim: SMS Failed: LenovoK3 API 1");
 
-		try {
+            boolean sentWithReflection = sendMultipartTextSMS(ctx, simID, toNum, null, smsTextlist, sentIntentList,
+                    deliveryIntentList);
 
-			// SubscriptionManager.getActiveSubId()
-			// SubscriptionManager.getActiveSubInfoList()
+            if (sentWithReflection) {
+                //Util.logActivities(ctx, "DualSim: Message to(" + toNum + ") Sent With Reflection API 1");
+                return true;
+            }
+            Util.logActivities(ctx, "SMS Failed: Reflection API 1");
 
-			long[] subIds = (long[]) invokeStaticMethod(SubscriptionManager.class, "getSubId", simID);
+        }
 
-			long subId;
-			if (subIds != null) {
-				subId = subIds[0];
-			} else {
-				subId = (long) invokeStaticMethod(SubscriptionManager.class, "getDefaultSubId");
-			}
+        Util.logActivities(ctx, "Failed to send SMS to(" + toNum + ")  with DualSimAPi..Falling back to default");
+        sendSMSWithDefault(toNum, smsTextlist, sentIntentList, deliveryIntentList);
+        return true;
+    }
 
-			SmsManager sms = (SmsManager) invokeStaticMethod(SmsManager.class, "getSmsManagerForSubscriber", subId);
+    private static boolean sentSMSWithLenovoK3API(Context ctx, int simID, String toNum, ArrayList<String> smsTextlist,
+                                                  ArrayList<PendingIntent> sentIntentList, ArrayList<PendingIntent> deliveryIntentList) {
 
-			sms.sendMultipartTextMessage(toNum, null, smsTextlist, sentIntentList, deliveryIntentList);
+        try {
 
-			return true;
-		} catch (Exception e) {
-			Util.logActivities(ctx, "Failed To Send SMS to(" + toNum + ") With Dual Sim: " + e.getMessage());
-		}
+            // SubscriptionManager.getActiveSubId()
+            // SubscriptionManager.getActiveSubInfoList()
 
-		return false;
-	}
+            long[] subIds = (long[]) invokeStaticMethod(SubscriptionManager.class, "getSubId", simID);
 
-	private static boolean sendMultipartTextSMS(Context ctx, int simID, String toNum, String centerNum,
-			ArrayList<String> smsTextlist, ArrayList<PendingIntent> sentIntentList,
-			ArrayList<PendingIntent> deliveryIntentList) {
-		String name;
-		try {
-			if (simID == 0) {
-				name = "isms";
-				// for model : "Philips T939" name = "isms0"
-			} else if (simID == 1) {
-				name = "isms2";
-			} else {
-				throw new Exception("can not get service which for sim '" + simID + "', only 0,1 accepted as values");
-			}
+            long subId;
+            if (subIds != null) {
+                subId = subIds[0];
+            } else {
+                subId = (long) invokeStaticMethod(SubscriptionManager.class, "getDefaultSubId");
+            }
 
-			Method method = Class.forName("android.os.ServiceManager").getDeclaredMethod("getService", String.class);
-			method.setAccessible(true);
-			Object param = method.invoke(null, name);
+            SmsManager sms = (SmsManager) invokeStaticMethod(SmsManager.class, "getSmsManagerForSubscriber", subId);
 
-			method = Class.forName("com.android.internal.telephony.ISms$Stub").getDeclaredMethod("asInterface",
-					IBinder.class);
-			method.setAccessible(true);
-			Object stubObj = method.invoke(null, param);
-			if (Build.VERSION.SDK_INT < 18) {
-				method = stubObj.getClass().getMethod("sendMultipartText", String.class, String.class, List.class,
-						List.class, List.class);
-				method.invoke(stubObj, toNum, centerNum, smsTextlist, sentIntentList, deliveryIntentList);
-			} else {
-				method = stubObj.getClass().getMethod("sendMultipartText", String.class, String.class, String.class,
-						List.class, List.class, List.class);
-				method.invoke(stubObj, ctx.getPackageName(), toNum, centerNum, smsTextlist, sentIntentList,
-						deliveryIntentList);
-			}
-			return true;
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-			Log.e("apipas", "ClassNotFoundException:" + e.getMessage());
-		} catch (NoSuchMethodException e) {
-			e.printStackTrace();
-			Log.e("apipas", "NoSuchMethodException:" + e.getMessage());
-		} catch (InvocationTargetException e) {
-			e.printStackTrace();
-			Log.e("apipas", "InvocationTargetException:" + e.getMessage());
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-			Log.e("apipas", "IllegalAccessException:" + e.getMessage());
-		} catch (Exception e) {
-			e.printStackTrace();
-			Log.e("apipas", "Exception:" + e.getMessage());
-		}
-		return false;
-	}
+            sms.sendMultipartTextMessage(toNum, null, smsTextlist, sentIntentList, deliveryIntentList);
 
-	public static boolean isDualSim(Context context) {
-		return getSIMInfo(context).size() > 1;
-	}
+            return true;
+        } catch (Exception e) {
+            Util.logActivities(ctx, "Failed To Send SMS to(" + toNum + ") With Dual Sim: " + e.getMessage());
+        }
 
-	public static List<SimInfo> getSIMInfo(Context context) {
-		List<SimInfo> simInfoList = new ArrayList<>();
-		Uri URI_TELEPHONY = Uri.parse("content://telephony/siminfo/");
-		Cursor c = context.getContentResolver().query(URI_TELEPHONY, null, null, null, null);
-		if (null != c && c.moveToFirst()) {
-			do {
-				int id = c.getInt(c.getColumnIndex("_id"));
+        return false;
+    }
 
-				int slot1 = c.getColumnIndex("slot");
-				slot1 = (slot1 == -1 ? c.getColumnIndex("sim_id") : slot1);
-				int slot = c.getInt(slot1);
+    private static boolean sendMultipartTextSMS(Context ctx, int simID, String toNum, String centerNum,
+                                                ArrayList<String> smsTextlist, ArrayList<PendingIntent> sentIntentList,
+                                                ArrayList<PendingIntent> deliveryIntentList) {
+        String name;
+        try {
+            if (simID == 0) {
+                name = "isms";
+                // for model : "Philips T939" name = "isms0"
+            } else if (simID == 1) {
+                name = "isms2";
+            } else {
+                throw new Exception("can not get service which for sim '" + simID + "', only 0,1 accepted as values");
+            }
 
-				String display_name = c.getString(c.getColumnIndex("display_name"));
-				String icc_id = c.getString(c.getColumnIndex("icc_id"));
-				SimInfo simInfo = new SimInfo(id, display_name, icc_id, slot);
+            Method method = Class.forName("android.os.ServiceManager").getDeclaredMethod("getService", String.class);
+            method.setAccessible(true);
+            Object param = method.invoke(null, name);
 
-				if (slot >= 0) {
-					simInfoList.add(simInfo);
-				}
-			} while (c.moveToNext());
+            method = Class.forName("com.android.internal.telephony.ISms$Stub").getDeclaredMethod("asInterface",
+                    IBinder.class);
+            method.setAccessible(true);
+            Object stubObj = method.invoke(null, param);
+            if (Build.VERSION.SDK_INT < 18) {
+                method = stubObj.getClass().getMethod("sendMultipartText", String.class, String.class, List.class,
+                        List.class, List.class);
+                method.invoke(stubObj, toNum, centerNum, smsTextlist, sentIntentList, deliveryIntentList);
+            } else {
+                method = stubObj.getClass().getMethod("sendMultipartText", String.class, String.class, String.class,
+                        List.class, List.class, List.class);
+                method.invoke(stubObj, ctx.getPackageName(), toNum, centerNum, smsTextlist, sentIntentList,
+                        deliveryIntentList);
+            }
+            return true;
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            Log.e("apipas", "ClassNotFoundException:" + e.getMessage());
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+            Log.e("apipas", "NoSuchMethodException:" + e.getMessage());
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+            Log.e("apipas", "InvocationTargetException:" + e.getMessage());
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+            Log.e("apipas", "IllegalAccessException:" + e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e("apipas", "Exception:" + e.getMessage());
+        }
+        return false;
+    }
 
-			c.close();
-		}
+    public static boolean isDualSim(Context context) {
+        return getSIMInfo(context).size() > 1;
+    }
 
-		return simInfoList;
-	}
+    public static List<SimInfo> getSIMInfo(Context context) {
+        List<SimInfo> simInfoList   = new ArrayList<>();
+        Uri           URI_TELEPHONY = Uri.parse("content://telephony/siminfo/");
+        Cursor        c             = context.getContentResolver().query(URI_TELEPHONY, null, null, null, null);
+        if (null != c && c.moveToFirst()) {
+            do {
+                int id = c.getInt(c.getColumnIndex("_id"));
+
+                int slot1 = c.getColumnIndex("slot");
+                slot1 = (slot1 == -1 ? c.getColumnIndex("sim_id") : slot1);
+                int slot = c.getInt(slot1);
+
+                String  display_name = c.getString(c.getColumnIndex("display_name"));
+                String  icc_id       = c.getString(c.getColumnIndex("icc_id"));
+                SimInfo simInfo      = new SimInfo(id, display_name, icc_id, slot);
+
+                if (slot >= 0) {
+                    simInfoList.add(simInfo);
+                }
+            } while (c.moveToNext());
+
+            c.close();
+        }
+
+        return simInfoList;
+    }
 
 }
